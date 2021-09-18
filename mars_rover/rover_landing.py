6f58a541cb3b7d4549d5ed2164999e2cf59c5327
ksuthar-at-335391035114 <ksuthar@virtuallogistics.ca>
2021-09-18 01:26:03 -0400

    Added rover_landing fiile

---
+++ mars_rover/rover_landing.py
@@ -0,0 +1,14 @@
+from .config import DIRECTIONS
+
+class RoverLanding:
+    def __init__(self, name, x, y, head='N'):
+        self.name = name
+        self.start_x = x
+        self.start_y = y
+        self.end_x = x
+        self.end_y = y
+        self.start_head = DIRECTIONS[head]
+        self.end_head = DIRECTIONS[head]
+
+    def __str__(self):
+        return f" Name : {self.name} \n ( {self.end_x, self.end_y, self.end_head})"
