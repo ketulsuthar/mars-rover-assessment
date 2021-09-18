1cc7a59bce6bf3bc2f2d9a2e7045dc1d51163a70
ksuthar-at-335391035114 <ksuthar@virtuallogistics.ca>
2021-09-18 01:25:06 -0400

    Added validate_move to validate current position of rover

--- mars_rover/plateau.py
+++ mars_rover/plateau.py
@@ -5,3 +5,6 @@ class Plateau():
         self._x = x
         self._y = y
 
+    def validate_move(self, cur_pos):
+        # print(cur_pos.end_x,cur_pos.end_y)
+        return cur_pos.end_x >= 0 and cur_pos.end_x <= self._x and cur_pos.end_y >= 0 and cur_pos.end_y <= self._y
